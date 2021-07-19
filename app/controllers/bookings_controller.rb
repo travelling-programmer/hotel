ba150087ef7ae91fb645523904bc79f84fa9695f
rwertzm05 <77314928+rwertzm05@users.noreply.github.com>
2021-07-19 18:20:21 -0500

    Fixed Error to show booking confirmaton
    Booking confirmation page now appears after booking a room

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -48,32 +48,31 @@ class BookingsController < ApplicationController
 
   # PATCH/PUT /bookings/1
   # PATCH/PUT /bookings/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @booking.update(booking_params_with_date)
-  #       format.html { redirect_to @booking, notice: 'Booking was successfully updated.' }
-  #       format.json { render :show, status: :ok, location: @booking }
-  #     else
-  #       format.html { render :edit }
-  #       format.json { render json: @booking.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
+  def update
+    respond_to do |format|
+      if @booking.update(booking_params_with_date)
+        format.html { redirect_to @booking, notice: 'Booking was successfully updated.' }
+        format.json { render :show, status: :ok, location: @booking }
+      else
+        format.html { render :edit }
+        format.json { render json: @booking.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
-  # # DELETE /bookings/1
-  # # DELETE /bookings/1.json
-  # def destroy
-  #   @booking.destroy
-  #   respond_to do |format|
-  #     format.html { redirect_to bookings_url, notice: 'Booking was successfully destroyed.' }
-  #     format.json { head :no_content }
-  #   end
-  # end
+  # DELETE /bookings/1
+  # DELETE /bookings/1.json
+  def destroy
+    @booking.destroy
+    respond_to do |format|
+      format.html { redirect_to bookings_url, notice: 'Booking was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_booking
-      debugger
+    def set_booking      
       @booking = current_user.bookings.find_by_id(params[:id])
     end
 
